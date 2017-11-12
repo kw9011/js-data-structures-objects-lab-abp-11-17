e460bc1a697a475009c766de9ada4b14fee260b4
Karmy Widjaja <karmy0311@gmail.com>
2017-11-12 15:11:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ function updateDriverWithKeyAndValue(driver,key,value){
   return newdriver;
 }
 function destructivelyUpdateDriverWithKeyAndValue (driver,key,value){
-  driver[key]=value; 
+  driver[key]=value;
   return driver;
 }
-
-
 
 function deleteFromDriverByKey(driver, key) {
   const newDriver = Object.assign({}, driver);
   delete newDriver[key];
   return newDriver;
+}
+function destructivelyDeleteFromDriverByKey(driver,key){
+  delete driver[key];
+  return driver;
 }
