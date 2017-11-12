8ebd97330ac5aba20aacdf2218fb82f3156a49d1
Karmy Widjaja <karmy0311@gmail.com>
2017-11-12 14:22:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function drivers () {
   typeof driver.'object'
 }
+
+describe('updatedriverWithKeyAndValue(driver, key, value)', function () {
+    beforeEach(function () {
+      for (const key in driver) {
+        delete driver[key];
